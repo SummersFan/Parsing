2c4d70c37e2ec9ccaa4c4c43d9c17cbc97148816
SummersFan <978649973@qq.com>
2017-11-26 12:45:15 +0800

    预测分析表

--- Parsing/Parsing.js
+++ Parsing/Parsing.js
@@ -69,7 +69,7 @@ Vn.map(function (value) {
     follow[value] = [];
 });
 
-//求first集
+//求first集函数
 let getFirst = function (value, callback) {
 
     G.map(function (value1) {   //一行一行分析
@@ -153,12 +153,11 @@ let getFirst = function (value, callback) {
     })
 };
 
-//求follow集
+//求follow集函数
 let getFollow = function (value) {
-    if(follow[value].indexOf('#') === -1) {
+    if (follow[value].indexOf('#') === -1) {
         follow[value].push('#');    //先加入#
     }
-
 
 
     G.map(function (value1) {   //一行一行分析
@@ -167,86 +166,86 @@ let getFollow = function (value) {
         let valueAllSplit = value1ArrRight.split('|');  //分割'|'
 
 
-        if(value1.indexOf(value)){      //对需要的一行进行分析
+        if (value1.indexOf(value)) {      //对需要的一行进行分析
             for (let i in valueAllSplit) {
                 let arr = valueAllSplit[i];//有的箭头右部
 
                 let brr = [];
-                for(let j in arr){
-                    if(arr[j] === '\''){
-                        let str = arr[j-1] + '\'';
+                for (let j in arr) {
+                    if (arr[j] === '\'') {
+                        let str = arr[j - 1] + '\'';
 
                         brr.pop();
                         brr.push({str});
-                        j+=1;
-                    }else{
+                        j += 1;
+                    } else {
                         brr.push(arr[j]);
                     }
                 }
 
-                for(let j in brr){
-                    j= parseInt(j); //改变类型
+                for (let j in brr) {
+                    j = parseInt(j); //改变类型
 
-                    let char,char1,char2;
-                    if(typeof brr[j] === "object"){
+                    let char, char1, char2;
+                    if (typeof brr[j] === "object") {
                         char = brr[j].str;
-                    }else{
+                    } else {
                         char = brr[j];
                     }
 
-                    if(typeof brr[j+1] === "object"){
-                        char1 = brr[j+1].str;
-                    }else{
-                        char1 = brr[j+1];
+                    if (typeof brr[j + 1] === "object") {
+                        char1 = brr[j + 1].str;
+                    } else {
+                        char1 = brr[j + 1];
                     }
 
-                    if(typeof brr[j-1] === "object"){
-                        char2 = brr[j-1].str;
-                    }else{
-                        char2 = brr[j-1];
+                    if (typeof brr[j - 1] === "object") {
+                        char2 = brr[j - 1].str;
+                    } else {
+                        char2 = brr[j - 1];
                     }
 
-                    if(char === value && Vn.indexOf(char) !== -1 && Vt.indexOf(char1) !== -1){
+                    if (char === value && Vn.indexOf(char) !== -1 && Vt.indexOf(char1) !== -1) {
 
-                        if(follow[value].indexOf(char1) === -1) {
+                        if (follow[value].indexOf(char1) === -1) {
                             follow[value].push(char1);
                         }
                     }
 
 
-                    if (char === value && j > 0 && j < brr.length  && Vn.indexOf(char) !== -1 && Vn.indexOf(char1) !== -1 ) {   //对于产生式：A->aBC,将除去空集e的First（C）加入Follow（B）中;
+                    if (char === value && j > 0 && j < brr.length && Vn.indexOf(char) !== -1 && Vn.indexOf(char1) !== -1) {   //对于产生式：A->aBC,将除去空集e的First（C）加入Follow（B）中;
 
 
                         for (let k in first[char1]) {  //第一个非终结符号的first集合加入
                             if (follow[char].indexOf(first[char1][k]) === -1 && first[char1][k] !== 'ε') {
 
-                                if(follow[value].indexOf(first[char1][k]) === -1) {
+                                if (follow[value].indexOf(first[char1][k]) === -1) {
                                     follow[value].push(first[char1][k]);
                                 }
 
                             }
                         }
 
-                        if(first[char1].indexOf('ε') !== -1){
+                        if (first[char1].indexOf('ε') !== -1) {
 
                             getFollow(value1ArrLeft);
 
-                            for(let k in follow[value1ArrLeft]){
-                                if(follow[value].indexOf(follow[value1ArrLeft][k]) === -1 ){
+                            for (let k in follow[value1ArrLeft]) {
+                                if (follow[value].indexOf(follow[value1ArrLeft][k]) === -1) {
                                     follow[value].push(follow[value1ArrLeft][k]);
                                 }
                             }
                             return;
                         }
-                    }else if(char === value && Vn.indexOf(char) !== -1 && j === brr.length-1){
+                    } else if (char === value && Vn.indexOf(char) !== -1 && j === brr.length - 1) {
 
-                        if(first[char].indexOf('ε') !== -1){
+                        if (first[char].indexOf('ε') !== -1) {
 
 
                             getFollow(value1ArrLeft);     //对需要的那一行进行分析防止递归的堆栈溢出错误
 
-                            for(let k in follow[value1ArrLeft]){
-                                if(follow[value].indexOf(follow[value1ArrLeft][k]) === -1 ){
+                            for (let k in follow[value1ArrLeft]) {
+                                if (follow[value].indexOf(follow[value1ArrLeft][k]) === -1) {
                                     follow[value].push(follow[value1ArrLeft][k]);
                                 }
                             }
@@ -262,10 +261,109 @@ let getFollow = function (value) {
 
 };
 
+//判断LL(1)文法函数
+let chargeLL1 = function () {
+    let flag = 0;    //LL(1)文法判断标志
+    //条件1不含左递归
+    //条件2文法中每一个产生式的候选集两两不相交
+    //条件3first(A)与follow(A)不想交
+    G.map(function (value1) {
+        let value1ArrLeft = value1.split(['->'])[0];
+        let value1ArrRight = value1.split('->')[1];
+        let valueAllSplit = value1ArrRight.split('|');  //分割'|'
+
+        for (let i in valueAllSplit) {
+            let arr = valueAllSplit[i];//有的箭头右部
+
+
+            let brr = [];
+            for (let j in arr) {
+                if (arr[j] === '\'') {
+                    let str = arr[j - 1] + '\'';
+
+                    brr.pop();
+                    brr.push({str});
+                    j += 1;
+                } else {
+                    brr.push(arr[j]);
+                }
+            }
+
+            for (let j in brr) {
+                j = parseInt(j); //改变类型
+
+                let char, char1, char2;
+                if (typeof brr[j] === "object") {
+                    char = brr[j].str;
+                } else {
+                    char = brr[j];
+                }
+
+                if (typeof brr[j + 1] === "object") {
+                    char1 = brr[j + 1].str;
+                } else {
+                    char1 = brr[j + 1];
+                }
+
+                if (typeof brr[j - 1] === "object") {
+                    char2 = brr[j - 1].str;
+                } else {
+                    char2 = brr[j - 1];
+                }
+
+
+
+                for (let k = j + 1; k < brr.length; k++) {  //条件2
+                    if (typeof brr[k] === "object") {
+                        char1 = brr[k].str;
+                    } else {
+                        char1 = brr[k];
+                    }
+
+                    for(let l in first[char]){
+                        if(first[char1] === undefined|| first[char1] === undefined){
+                            continue;
+                        }
+                        if(first[char1].indexOf(first[char][l]) !== -1){
+                            flag = -1;
+                            return -1;
+                        }
+                    }
+                }
+            }
+
+
+        }
+
+        for(let i in first[value1ArrLeft]){
+
+            if(follow[value1ArrLeft].indexOf(first[value1ArrLeft][i]) !== -1 ){
+
+                flag = -1;
+                return -1;
+            }
+        }
+    });
+    return flag;
+};
+
+//预测分析表构造函数
+let getPredictAnalyticalTable = function () {
+  let arr = new Array(Vt.length+1);
+
+  for(let i = 0 ;i < arr.length+1;i++){
+      arr[i]= new Array(Vn.length+1);
+      for(let j=0;j<Vn.length;j++){      //二维长度为5
+          arr[i][j]=1;
+      }
+
+  }
+  console.log(arr);
+};
+
 //非终结符求first集
 Vn.map(function (value) {
     getFirst(value);
-
 });
 
 //非终结符求follow集
@@ -273,13 +371,24 @@ Vn.map(function (value) {
     getFollow(value);
 });
 
+// first['T\''].push('i');  //LL(1)文法的错误判断1
+// follow['T\''].push('ε');  //LL(1)文法的错误判断2
 
+
+getPredictAnalyticalTable();
+console.log('文法消除左递归之后的文法G:');
 console.log(G);
 console.log('-----------------分界线-----------------');
 console.log('first:');
 console.log(first);
 console.log('follow:');
 console.log(follow);
-
+console.log('-----------------分界线-----------------');
+let flag = chargeLL1();
+if(flag === -1){
+    console.log("该文法不是LL(1)文法");
+}else{
+    console.log("该文法是LL(1)文法,开始进行预测分析表的构造....");
+}
 
 
